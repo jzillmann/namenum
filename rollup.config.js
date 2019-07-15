901cbb1a4ea65001eabbdb2a779d3535b3f79790
Johannes Zillmann <jz@datameer.com>
2019-07-15 22:37:01 +0200

    Reformat files
    * Replace tabs with spaces

--- rollup.config.js
+++ rollup.config.js
@@ -7,52 +7,52 @@ import { terser } from 'rollup-plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			},
+    input: 'src/main.js',
+    output: {
+        sourcemap: true,
+        format: 'iife',
+        name: 'app',
+        file: 'public/bundle.js'
+    },
+    plugins: [
+        svelte({
+            // enable run-time checks when not in production
+            dev: !production,
+            // we'll extract any component CSS out into
+            // a separate file — better for performance
+            css: css => {
+                css.write('public/bundle.css');
+            },
 
-			onwarn: (warning, handler) => {
-				// e.g. don't warn on autofocus
-				if (warning.code === 'a11y-autofocus') return;
+            onwarn: (warning, handler) => {
+                // e.g. don't warn on autofocus
+                if (warning.code === 'a11y-autofocus') return;
 
-				// let Rollup handle all other warnings normally
-				handler(warning);
-			}
-		}),
+                // let Rollup handle all other warnings normally
+                handler(warning);
+            }
+        }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-		commonjs(),
+        // If you have external dependencies installed from
+        // npm, you'll most likely need these plugins. In
+        // some cases you'll need additional configuration —
+        // consult the documentation for details:
+        // https://github.com/rollup/rollup-plugin-commonjs
+        resolve({
+            browser: true,
+            dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
+        }),
+        commonjs(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+        // Watch the `public` directory and refresh the
+        // browser on changes when not in production
+        !production && livereload('public'),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+        // If we're building for production (npm run build
+        // instead of npm run dev), minify
+        production && terser()
+    ],
+    watch: {
+        clearScreen: false
+    }
 };
